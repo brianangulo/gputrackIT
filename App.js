7fe96c013fd3cf402b7119fa0001059bdf11bf3d
Brian Angulo <brianangulo96@gmail.com>
2021-04-27 15:19:45 -0400

    Stack navigation setting up

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
 import Main from "./components/MainComponent";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
+    <NavigationContainer>
       <Main />
+    </NavigationContainer>
   );
 }
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,6 +1,39 @@
 import React, { Component } from 'react';
 import { View, Text } from "react-native";
 import TrackIT from "./TrackITComponent";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Icon } from "react-native-elements";
+
+const Stack = createStackNavigator();
+
+const MainStack = () => {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen
+          name="TrackIT"
+          component={TrackIT}
+          options={{
+            headerTitleStyle: {
+                color: "#ffffff",
+            },  
+            headerStyle: {
+              backgroundColor: "#2459E0",
+            },
+            headerTitleAlign: "center",
+            headerLeft: () => (
+                <Icon
+                name="bars"
+                type="font-awesome-5"
+                color="#ffffff"
+                iconStyle={{padding: 15}}
+                onPress= {() => {console.log("Drawer menu icon was clicked");}}
+                />
+            )
+          }}
+        />
+      </Stack.Navigator>
+    );
+}
 
 class Main extends Component {
     constructor(props) {
@@ -11,7 +44,7 @@ class Main extends Component {
     }
     render() {
         return (
-            <TrackIT />
+            <MainStack />
         );
     }
 }
