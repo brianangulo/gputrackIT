6e8d10c0dea43f7fbe4a7a121d8a1b32551facb8
Brian Angulo <brianangulo96@gmail.com>
2021-04-27 13:54:15 -0400

    Working GPU picker

--- components/TrackITComponent.js
+++ components/TrackITComponent.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
-import { View, Text, TextInput, StyleSheet } from "react-native";
+import { View, Text, TextInput, StyleSheet, ScrollView } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 
 class TrackIT extends Component {
   constructor(props) {
+    super(props);
     this.state = {
         selectedValue: "",
     };
@@ -11,20 +12,23 @@ class TrackIT extends Component {
 
   render() {
     return (
-      <View>
-          <Text style={styles.pickerTitle}>Selected a GPU to be notified for: </Text>
-          <Picker
-          style={styles.picker}
+      <View style={styles.container}>
+        <Text style={styles.pickerTitle}>
+          Select a GPU to be notified for:{" "}
+        </Text>
+        <Picker
           selectedValue={this.state.selectedValue}
           onValueChange={(itemValue, itemIndex) => {
             console.log("Selected " + itemValue);
+            this.setState({ selectedValue: itemValue });
           }}
-          >
-              <Picker.Item label="Nvidia RTX 3060" value="rtx3060" />
-              <Picker.Item label="Nvidia RTX 3070" value="rtx3070" />
-              <Picker.Item label="Nvidia RTX 3080" value="rtx3080" />
-              <Picker.Item label="Nvidia RTX 3090" value="rtx3090" />
-          </Picker>
+          style={styles.picker}
+        >
+          <Picker.Item label="Nvidia RTX 3060" value="rtx3060" />
+          <Picker.Item label="Nvidia RTX 3070" value="rtx3070" />
+          <Picker.Item label="Nvidia RTX 3080" value="rtx3080" />
+          <Picker.Item label="Nvidia RTX 3090" value="rtx3090" />
+        </Picker>
       </View>
     );
   }
@@ -32,13 +36,21 @@ class TrackIT extends Component {
 
 const styles = StyleSheet.create({
   pickerTitle: {
-    fontSize: 20,
+    fontSize: 18,
     fontWeight: "bold",
-    flex: 2,
+    alignItems: "center",
   },
   picker: {
-      flex: 2,
-  }
+    color: "#ffffff",
+    backgroundColor: "#2459E0",
+  },
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    alignContent: "center",
+    justifyContent: "center",
+    padding: 30,
+  },
 });
 
 export default TrackIT;
