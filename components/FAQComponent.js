1613b4d22df4384df4c1f55073375dc28838bebc
Brian Angulo <brianangulo96@gmail.com>
2021-04-28 18:17:43 -0400

    Fetching FAQ Comp information from firestore

--- components/FAQComponent.js
+++ components/FAQComponent.js
@@ -1,37 +1,112 @@
 import React, { Component } from "react";
 import { Card } from "react-native-elements";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, LogBox } from "react-native";
+import { db, auth } from "../firebase/firebase";
 
-const FAQ = () => {
-  return (
-    <View>
-      <Card>
-        <Card.Title>FAQs</Card.Title>
-        <Card.Divider />
-        <Text style={styles.subtitle}>How does GPUTrackIT work?{"\n"}</Text>
-        <Text>
-          GPUTrackIT will track the selected GPU on the Best Buy online store
-          and will notify you via email once the card goes on stock.{"\n"}
-        </Text>
-        <Text style={styles.subtitle}>
-          What email will GPUTrackIT use to send notifications to?{"\n"}
-        </Text>
-        <Text>The email found in your Google account used to sign up</Text>
-      </Card>
-      <Card>
-        <Card.Title>Bug Report/Developer Contact</Card.Title>
-        <Card.Divider />
-        <Text style={styles.subtitle}>Email:{"\n"}</Text>
-        <Text>bangulo219@gmail.com</Text>
-      </Card>
-    </View>
-  );
-};
+const faqRef = db.collection("faq");
+
+class FAQ extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      faqInfo: [],
+
+     };
+  }
+
+  componentDidMount() {
+    console.log("Mounted");
+    faqRef.get().then(
+      (snapshot) => {
+        const info = []
+        snapshot.forEach(
+          doc => {
+            const data = doc.data()  
+            info.push(data)
+          }
+        )
+        this.setState({faqInfo: info})
+        console.log(this.state.faqInfo)
+      }
+    ).catch(error => console.log(error))
+  }
+
+  render() {
+    return (
+      <View>
+        <Card>
+          <Card.Title>FAQs</Card.Title>
+          <Card.Divider />
+          <Text style={styles.subtitle}>
+            {this.state.faqInfo.map((subtitle) => {
+              return subtitle.subtitle1;
+            })}
+            {"\n"}
+          </Text>
+          <Text>
+            {this.state.faqInfo.map((subtitle) => {
+              return subtitle.answer1;
+            })}
+            {"\n"}
+          </Text>
+          <Text style={styles.subtitle}>
+            {this.state.faqInfo.map((subtitle) => {
+              return subtitle.subtitle2;
+            })}
+            {"\n"}
+          </Text>
+          <Text>
+            {this.state.faqInfo.map((subtitle) => {
+              return subtitle.answer2;
+            })}
+          </Text>
+        </Card>
+        <Card>
+          <Card.Title>Bug Report/Developer Contact</Card.Title>
+          <Card.Divider />
+          <Text style={styles.subtitle}>
+            {this.state.faqInfo.map((subtitle) => {
+              return subtitle.contacttype;
+            })}
+            {"\n"}
+          </Text>
+          <Text>
+            {this.state.faqInfo.map((subtitle) => {
+              return subtitle.email;
+            })}
+          </Text>
+        </Card>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
     subtitle: {
         fontWeight: "bold",
     },
 })
+
+{/* <View>
+        <Card>
+          <Card.Title>FAQs</Card.Title>
+          <Card.Divider />
+          <Text style={styles.subtitle}>How does GPUTrackIT work?{"\n"}</Text>
+          <Text>
+            GPUTrackIT will track the selected GPU on the Best Buy online store
+            and will notify you via email once the card goes on stock.{"\n"}
+          </Text>
+          <Text style={styles.subtitle}>
+            What email will GPUTrackIT use to send notifications to?{"\n"}
+          </Text>
+          <Text>The email found in your Google account used to sign up</Text>
+        </Card>
+        <Card>
+          <Card.Title>Bug Report/Developer Contact</Card.Title>
+          <Card.Divider />
+          <Text style={styles.subtitle}>Email:{"\n"}</Text>
+          <Text>bangulo219@gmail.com</Text>
+        </Card>
+      </View> */}
 
 export default FAQ;
