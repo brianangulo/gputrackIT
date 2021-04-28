1e1c2299db968d972ea79f6ccf411010dd850dd2
Brian Angulo <brianangulo96@gmail.com>
2021-04-28 18:18:37 -0400

    Deleted comment

--- components/FAQComponent.js
+++ components/FAQComponent.js
@@ -87,26 +87,4 @@ const styles = StyleSheet.create({
     },
 })
 
-{/* <View>
-        <Card>
-          <Card.Title>FAQs</Card.Title>
-          <Card.Divider />
-          <Text style={styles.subtitle}>How does GPUTrackIT work?{"\n"}</Text>
-          <Text>
-            GPUTrackIT will track the selected GPU on the Best Buy online store
-            and will notify you via email once the card goes on stock.{"\n"}
-          </Text>
-          <Text style={styles.subtitle}>
-            What email will GPUTrackIT use to send notifications to?{"\n"}
-          </Text>
-          <Text>The email found in your Google account used to sign up</Text>
-        </Card>
-        <Card>
-          <Card.Title>Bug Report/Developer Contact</Card.Title>
-          <Card.Divider />
-          <Text style={styles.subtitle}>Email:{"\n"}</Text>
-          <Text>bangulo219@gmail.com</Text>
-        </Card>
-      </View> */}
-
 export default FAQ;
