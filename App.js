12bf056f04c650ec3e44a48ad2a6525c598a207c
Brian Angulo <brianangulo96@gmail.com>
2021-04-29 18:16:02 -0400

    Initial redux set up

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
 import Main from "./components/MainComponent";
-import { NavigationContainer } from "@react-navigation/native";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <Provider store={store} >
       <Main />
-    </NavigationContainer>
+    </Provider>
   );
 }
