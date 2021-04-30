af0d095a13e75f803e4266113f59e7a9a8df37d9
Brian Angulo <brianangulo96@gmail.com>
2021-04-30 16:20:32 -0400

    Setting up price view and logic

--- components/TrackITComponent.js
+++ components/TrackITComponent.js
@@ -3,18 +3,23 @@ import { View, Text, StyleSheet } from "react-native";
 import { Button } from "react-native-elements";
 import { Picker } from "@react-native-picker/picker";
 import { useDispatch, useSelector } from "react-redux";
-import { selectValue } from "../redux/appSlice";
+import { selectValue, setGpu, setPrice } from "../redux/appSlice";
 
 function TrackIT() {
   const dispatch = useDispatch();
   const selectedValue = useSelector((state) => state.app.selectedValue);
+  const price = useSelector((state) => state.app.price);
+  const gpu = useSelector((state) => state.app.gpu);
 
     return (
       <View style={styles.container}>
         <View style={{ flex: 0.4, marginTop: 70 }}>
           <Text style={styles.pickerTitle}>
-            Select a GPU to be notified for:{" "}
+            Select a GPU to be shown its current price:{" "}
           </Text>
+        </View>
+        <View style={styles.priceView}>
+          <Text style={styles.priceText}>Current {gpu} price is: {price}</Text>
         </View>
         <View style={{ flex: 0.5 }}>
           <Picker
@@ -22,13 +27,14 @@ function TrackIT() {
             onValueChange={(itemValue, itemIndex) => {
               console.log("Selected " + itemValue)
               dispatch(selectValue(itemValue))
+              dispatch(setGpu(itemValue)); 
             }}
             style={styles.picker}
           >
-            <Picker.Item label="Nvidia RTX 3060" value="rtx3060" />
-            <Picker.Item label="Nvidia RTX 3070" value="rtx3070" />
-            <Picker.Item label="Nvidia RTX 3080" value="rtx3080" />
-            <Picker.Item label="Nvidia RTX 3090" value="rtx3090" />
+            <Picker.Item label="Nvidia RTX 3060" value="RTX3060" />
+            <Picker.Item label="Nvidia RTX 3070" value="RTX3070" />
+            <Picker.Item label="Nvidia RTX 3080" value="RTX3080" />
+            <Picker.Item label="Nvidia RTX 3090" value="RTX3090" />
           </Picker>
         </View>
         <View style={{ flex: 1 }}>
@@ -37,6 +43,7 @@ function TrackIT() {
             title="Submit"
             onPress={() => {
               console.log("Submit button pressed");
+              dispatch(setPrice(10)); 
             }}
           />
         </View>
@@ -60,6 +67,13 @@ const styles = StyleSheet.create({
     alignContent: "center",
     justifyContent: "center",
     padding: 30,
+  },
+  priceView: {
+    flex: 0.2,
+  },
+  priceText: {
+    fontWeight: "bold",
+    fontSize: 15,
   },
 });
 
--- redux/appSlice.js
+++ redux/appSlice.js
@@ -5,17 +5,25 @@ export const appSlice = createSlice({
   initialState: {
     faqInfo: [],
     selectedValue: "",
+    price: 0,
+    gpu: "RTX3060",
   },
   reducers: {
-      selectValue: (state, action) => {
-          state.selectedValue = action.payload
-      },
-      getFaqs: (state, action) => {
-        state.faqInfo = action.payload
-      }
-  }
+    selectValue: (state, action) => {
+      state.selectedValue = action.payload;
+    },
+    getFaqs: (state, action) => {
+      state.faqInfo = action.payload;
+    },
+    setPrice: (state, action) => {
+      state.price = state.price + action.payload;
+    },
+    setGpu: (state, action) => {
+      state.gpu = action.payload;
+    }
+  },
 });
 
-export const { selectValue, getFaqs } = appSlice.actions;
+export const { selectValue, getFaqs, setGpu, setPrice } = appSlice.actions;
 
 export default appSlice.reducer;
