052c60e789b6854f283355c3d636bdad00a89cb7
Brian Angulo <brianangulo96@gmail.com>
2021-06-03 23:09:57 -0400

    Mailer setup and ios alerts

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button, ToastAndroid } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Button, ToastAndroid, Platform, Alert } from 'react-native';
 import { Input } from "react-native-elements";
 import { useForm, Controller } from "react-hook-form";
 import { db } from "../firebase/firebase";
@@ -15,9 +15,43 @@ function Contact() {
       reset,
       formState: { errors },
     } = useForm();
+    //Firebase Url reference
+    const url = db.collection("url").doc("url");
+    //hook to hold it
+    const [mailerURL, setMailerUrl] = useState("");
+    //hook to update on mount
+    useEffect(
+      () => {
+        //Getting post url from FB
+        url
+          .get()
+          .then((doc) => {
+            setMailerUrl(doc.data().url);
+          })
+          .catch(console.error);
+      }, []
+    )
+
+    //Mailer notification
+     const sendMail = (message, url, subject) => {
+       const data = {
+         email: `bangulo219@gmail.com`,
+         subject: "Message received via GPUTrackIT",
+         message: `You have received a message with the following subject: ${subject}. And the following body: ${message}`,
+       };
+       fetch(url, {
+         method: "POST",
+         mode: "cors",
+         headers: {
+           Accept: "application/json",
+           "Content-Type": "application/json",
+         },
+         body: JSON.stringify(data),
+       }).catch(console.error);
+     };
 
     const onSubmit = (data) => {
-      
+
       console.log(data); 
       
       db.collection("contactus")
@@ -28,16 +62,26 @@ function Contact() {
         })
         .then(() => {
           console.log("Document successfully written!");
-          ToastAndroid.show("Message succesfully sent", ToastAndroid.LONG)
+         if (Platform.OS === "ios") {
+            Alert.alert("Message succesfully sent!");
+         } else {
+           ToastAndroid.show("Message succesfully sent", ToastAndroid.LONG);
+         }
           reset()
         })
         .catch((error) => {
           console.error("Error writing document: ", error);
-          ToastAndroid.show(
-            `Error writing document: ${error}`,
-            ToastAndroid.LONG
-          );
+           if (Platform.OS === "ios") {
+             Alert.alert(`Error writing document: ${error}`);
+           } else {
+             ToastAndroid.show(
+               `Error writing document: ${error}`,
+               ToastAndroid.LONG
+             );
+           }
+          
         });
+        sendMail(data.body, mailerURL, data.subject);
     }
 
     return (
